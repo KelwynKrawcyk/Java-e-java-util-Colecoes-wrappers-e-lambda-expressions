74460f97b4eb7bdc2fcb4580db469c63a62d12a7
Kelwyn K <krawcykk1@gmail.com>
2025-09-24 09:16:14 -0300

    Adicionando guardador de contas

--- src/br/com/bytebank/banco/test/TesteArrayReferencias.java
+++ src/br/com/bytebank/banco/test/TesteArrayReferencias.java
@@ -1,19 +1,28 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.models.Conta;
 import br.com.bytebank.banco.models.ContaCorrente;
+import br.com.bytebank.banco.models.ContaPoupanca;
 
 public class TesteArrayReferencias {
     public static void main(String[] args) {
-        ContaCorrente[] contas = new ContaCorrente[5];
+
+        //Conta[] contas = new Conta[5];
+        Object[] referencias = new Conta[5];
+
+        //ContaCorrente[] contas = new ContaCorrente[5];
 
         ContaCorrente cc = new ContaCorrente(12, 14);
-        contas[0] = cc;
-        System.out.println(contas[0]);
+        referencias[0] = cc;
+        System.out.println(referencias[0]);
 
-        ContaCorrente cc2 = new ContaCorrente(41, 6);
-        contas[1] = cc2;
-        System.out.println(contas[1]);
+        ContaPoupanca cp = new ContaPoupanca(41, 6);
+        referencias[1] = cp;
+        System.out.println(referencias[1]);
 
-        System.out.println(contas[1].getNumero());
+        //System.out.println(referencias[1].getNumero());
+
+        ContaPoupanca ref = (ContaPoupanca) referencias[1]; //type cast
+
     }
 }
